87d90ac295d97bbe753184b3b40047f2aac1bd25
tonykosmos <a.kosmovskiy@gmail.com>
2018-09-29 16:57:11 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency>10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if (currency==0) return{};
+    var object = {};
+    let half = 0;
+    let quo = 0;
+    let ten = 0;
+    let five = 0;
+    let one = 0;
+
+    while (currency>=50){
+    currency = currency-50;
+     half++;
+     if (currency<50) break;
+    }
+    if (half!=0) object.H = half;
+    
+    while (currency>=25){
+        currency = currency-25;
+         quo++;
+         if (currency<25) break;
+    } 
+        if (quo!=0) object.Q = quo;
+
+    while (currency>=10){
+        currency = currency-10;
+         ten++;
+         if (currency<10) break;
+    } 
+        if (ten!=0) object.D = ten;
+
+    while (currency>=5){
+        currency = currency-5;
+         five++;
+         if (five<5) break;
+    } 
+
+        if (five!=0) object.N = five; 
+    while (currency>=1){
+        currency = currency-1;
+         one++;
+         if (currency<1) break;
+    } 
+    if (one!=0) object.P = one;
+    return object;    
 }
